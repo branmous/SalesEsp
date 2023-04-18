3dcc7e1635db09740acbd8fb7533b3fd1858439d
branmous <brandon-377@hotmail.com>
2023-04-17 19:15:02 -0500

    Create controller country

---
+++ Sales/Sales.API/Controllers/CountriesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sales.API.Data;
+using Sales.Shared.Entities;
+
+namespace Sales.API.Controllers
+{
+    [ApiController]
+    [Route("/api/countries")]
+    public class CountriesController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public CountriesController(DataContext dataContext)
+        {
+            this._dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsyc()
+        {
+            return Ok(await _dataContext.Countries.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsyc(Country country)
+        {
+            _dataContext.Add(country);
+            await _dataContext.SaveChangesAsync();
+            return Ok(country);
+        }
+    }
+}
---
+++ Sales/Sales.Shared/Entities/Country.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sales.Shared.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+
+        [Display(Name = "Name")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tenex máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Name { get; set; } = null!;
+    }
+}
