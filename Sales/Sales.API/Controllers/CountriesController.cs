353ebc9ff29f49a3b2274943945b98f7c052f7ab
branmous <brandon-377@hotmail.com>
2023-04-17 19:21:06 -0500

    Complete countries controller

--- Sales/Sales.API/Controllers/CountriesController.cs
+++ Sales/Sales.API/Controllers/CountriesController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http.HttpResults;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Sales.API.Data;
 using Sales.Shared.Entities;
@@ -23,12 +22,45 @@ namespace Sales.API.Controllers
             return Ok(await _dataContext.Countries.ToListAsync());
         }
 
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAsyc(int id)
+        {
+            var country = await _dataContext.Countries.FirstOrDefaultAsync(c => c.Id == id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+            return Ok(country);
+        }
+
         [HttpPost]
         public async Task<IActionResult> PostAsyc(Country country)
         {
             _dataContext.Add(country);
             await _dataContext.SaveChangesAsync();
             return Ok(country);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> PutAsyc(Country country)
+        {
+            _dataContext.Update(country);
+            await _dataContext.SaveChangesAsync();
+            return Ok(country);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAsyc(int id)
+        {
+            var country = await _dataContext.Countries.FirstOrDefaultAsync(c => c.Id == id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            _dataContext.Remove(country);
+            await _dataContext.SaveChangesAsync();
+            return NoContent();
         }
     }
 }
