59e07417c1e4c7803ff4074f8574a757f7a788ae
branmous <brandon-377@hotmail.com>
2023-04-23 11:11:46 -0500

    apies of states and cities

---
+++ Sales/Sales.API/Controllers/CitiesController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sales.API.Data;
+using Sales.Shared.Entities;
+
+namespace Sales.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public CitiesController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsyc()
+        {
+            return Ok(await _dataContext.Cities.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAsyc(int id)
+        {
+            var state = await _dataContext.Cities.FirstOrDefaultAsync(c => c.Id == id);
+            if (state == null)
+            {
+                return NotFound();
+            }
+            return Ok(state);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsyc(City city)
+        {
+            _dataContext.Add(city);
+            try
+            {
+                await _dataContext.SaveChangesAsync();
+                return Ok(city);
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                if (dbUpdateException.InnerException!.Message.Contains("duplicate"))
+                {
+                    return BadRequest("Ya existe una ciudad con el mismo nombre.");
+                }
+                else
+                {
+                    return BadRequest(dbUpdateException.InnerException.Message);
+                }
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message);
+            }
+
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> PutAsyc(City city)
+        {
+            try
+            {
+                _dataContext.Update(city);
+                await _dataContext.SaveChangesAsync();
+                return Ok(city);
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                if (dbUpdateException.InnerException!.Message.Contains("duplicate"))
+                {
+                    return BadRequest("Ya existe una ciudad con el mismo nombre.");
+                }
+                else
+                {
+                    return BadRequest(dbUpdateException.InnerException.Message);
+                }
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message);
+            }
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAsyc(int id)
+        {
+            var city = await _dataContext.Cities.FirstOrDefaultAsync(c => c.Id == id);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            _dataContext.Remove(city);
+            await _dataContext.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
---
+++ Sales/Sales.API/Controllers/StatesController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sales.API.Data;
+using Sales.Shared.Entities;
+
+namespace Sales.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatesController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public StatesController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsyc()
+        {
+            return Ok(await _dataContext.States.Include(c => c.Cities).ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAsyc(int id)
+        {
+            var state = await _dataContext.States.Include(c => c.Cities).FirstOrDefaultAsync(c => c.Id == id);
+            if (state == null)
+            {
+                return NotFound();
+            }
+            return Ok(state);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsyc(State state)
+        {
+            _dataContext.Add(state);
+            try
+            {
+                await _dataContext.SaveChangesAsync();
+                return Ok(state);
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                if (dbUpdateException.InnerException!.Message.Contains("duplicate"))
+                {
+                    return BadRequest("Ya existe un estado/departamento con el mismo nombre.");
+                }
+                else
+                {
+                    return BadRequest(dbUpdateException.InnerException.Message);
+                }
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message);
+            }
+
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> PutAsyc(State state)
+        {
+            try
+            {
+                _dataContext.Update(state);
+                await _dataContext.SaveChangesAsync();
+                return Ok(state);
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                if (dbUpdateException.InnerException!.Message.Contains("duplicate"))
+                {
+                    return BadRequest("Ya existe un estado/departamento con el mismo nombre.");
+                }
+                else
+                {
+                    return BadRequest(dbUpdateException.InnerException.Message);
+                }
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message);
+            }
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAsyc(int id)
+        {
+            var state = await _dataContext.States.FirstOrDefaultAsync(c => c.Id == id);
+            if (state == null)
+            {
+                return NotFound();
+            }
+
+            _dataContext.Remove(state);
+            await _dataContext.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
